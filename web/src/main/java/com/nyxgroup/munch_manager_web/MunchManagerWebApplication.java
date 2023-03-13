3abd15321c795e5ae725797390c0651068fb5225
Joe McLaughlin <JoeMcLaughlin@Joes-MacBook-Air.local>
2023-03-13 13:51:51 +0000

    main components added to local repo and react app working

---
+++ web/src/main/java/com/nyxgroup/munch_manager_web/MunchManagerWebApplication.java
@@ -0,0 +1,13 @@
+package com.nyxgroup.munch_manager_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunchManagerWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MunchManagerWebApplication.class, args);
+	}
+
+}
---
+++ web/src/test/java/com/nyxgroup/munch_manager_web/MunchManagerWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyxgroup.munch_manager_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MunchManagerWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
