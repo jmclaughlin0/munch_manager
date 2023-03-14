cd58b893a41b084f2bc85b0ccab4a9e578ccb568
Joe McLaughlin <JoeMcLaughlin@Joes-MacBook-Air.local>
2023-03-14 20:38:14 +0000

    caldendar added to eating and image upload added to poo tracker

--- frontend/src/FoodLog.js
+++ frontend/src/FoodLog.js
@@ -1,9 +1,26 @@
-import React from "react";
-import {Header} from "semantic-ui-react";
+import React, {useState} from "react";
+import {Header, Segment} from "semantic-ui-react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 
 export default function FoodLog(){
+    const [startDate, setStartDate] = useState(new Date());
+
     return(
-        <Header className={'App-header'} color='white' content={'Welcome to the Food Logger'}/>
+        <div className={'App-header'} align={'center'}>
+            <Header color='white' content={'Welcome to the Food Logger'}/>
+            <Segment size={'large'} inverted color={'blue'}>
+                <DatePicker showIcon
+                            dateFormat="dd/MM/yyyy H:mm"
+                            showTimeSelect
+                            timeIntervals={15}
+                            todayButton="Today"
+                            withPortal
+                            selected={startDate}
+                            onChange={(date) => setStartDate(date)}
+                />
+            </Segment>
+        </div>
     );
 }
--- frontend/src/PooClassifier.js
+++ frontend/src/PooClassifier.js
@@ -1,9 +1,19 @@
 import React from "react";
 import {Header} from "semantic-ui-react";
+import UploadImages from "./ImageUploading/UploadImages";
+import HeaderSubHeader from "semantic-ui-react/dist/commonjs/elements/Header/HeaderSubheader";
 
 
 export default function PooClassifier(){
     return(
-        <Header className={'App-header'} color='white' content={'Welcome to the Poo Paparazzi'}/>
+        <div>
+            <Header as={'h1'} className={'App-header'} color='white'>
+                Welcome to the Poo Paparazzi!
+
+                <HeaderSubHeader className={'App'}>Please Upload Your Photos Here:</HeaderSubHeader>
+                <UploadImages />
+            </Header>
+
+        </div>
     );
 }
